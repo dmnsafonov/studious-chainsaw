2037458f2e36baed6730e3bb08a19834f42b59bf
Dmitrii Safonov <domisafonov@gmail.com>
2024-07-14 22:47:42 -0300

    defer main ui until a buntton is pressed

--- app/src/main/kotlin/net/domisafonov/compasstestproject/ui/mainscreen/MainScreenUi.kt
+++ app/src/main/kotlin/net/domisafonov/compasstestproject/ui/mainscreen/MainScreenUi.kt
@@ -2,18 +2,23 @@ package net.domisafonov.compasstestproject.ui.mainscreen
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import net.domisafonov.compasstestproject.R
 import net.domisafonov.compasstestproject.ui.tenthcharacterscreen.TenthCharacterScreenUi
 import net.domisafonov.compasstestproject.ui.wordcountscreen.WordCountScreenUi
@@ -25,6 +30,30 @@ fun MainScreenUi(
     onWordCountClick: () -> Unit = {},
 ) {
 
+    val viewModel: MainScreenViewModel = hiltViewModel()
+
+    val isActivated by viewModel.isActivated.collectAsState()
+
+    if (isActivated) {
+        ActivatedUi(modifier, onTenthClick, onWordCountClick)
+    } else {
+        Box(modifier = modifier.fillMaxSize()) {
+            Button(
+                onClick = { viewModel.onButtonClick() },
+                modifier = Modifier.align(Alignment.Center)
+            ) {
+                Text(text = "Do it!")
+            }
+        }
+    }
+}
+
+@Composable
+private fun ActivatedUi(
+    modifier: Modifier,
+    onTenthClick: () -> Unit,
+    onWordCountClick: () -> Unit
+) {
     Column(
         modifier = modifier
             .fillMaxSize()
